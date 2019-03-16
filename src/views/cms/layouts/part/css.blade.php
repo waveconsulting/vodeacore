1eb7fa46b2f8b8b7587ab05a851ded33501f0f5e
Jovian Anderson <joviand.anderson@gmail.com>
2019-03-16 21:40:48 +0700

    change path to assets/vodea/vodeacore

--- src/views/cms/layouts/part/css.blade.php
+++ src/views/cms/layouts/part/css.blade.php
@@ -1,20 +1,20 @@
 <!-- plugins:css -->
-<link rel="stylesheet" href="{!! url('/') !!}/assets/vodeacore/vendors/iconfonts/mdi/css/materialdesignicons.min.css">
-<link rel="stylesheet" href="{!! url('/') !!}/assets/vodeacore/vendors/iconfonts/puse-icons-feather/feather.css">
-<link rel="stylesheet" href="{!! url('/') !!}/assets/vodeacore/vendors/css/vendor.bundle.base.css">
-<link rel="stylesheet" href="{!! url('/') !!}/assets/vodeacore/vendors/css/vendor.bundle.addons.css">
+<link rel="stylesheet" href="{!! url('/') !!}/assets/vodea/vodeacore/vendors/iconfonts/mdi/css/materialdesignicons.min.css">
+<link rel="stylesheet" href="{!! url('/') !!}/assets/vodea/vodeacore/vendors/iconfonts/puse-icons-feather/feather.css">
+<link rel="stylesheet" href="{!! url('/') !!}/assets/vodea/vodeacore/vendors/css/vendor.bundle.base.css">
+<link rel="stylesheet" href="{!! url('/') !!}/assets/vodea/vodeacore/vendors/css/vendor.bundle.addons.css">
 <!-- endinject -->
 <!-- plugin css for this page -->
-<link rel="stylesheet" href="{!! url('/') !!}/assets/vodeacore/vendors/iconfonts/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="{!! url('/') !!}/assets/vodea/vodeacore/vendors/iconfonts/font-awesome/css/font-awesome.min.css">
 
-<link rel="stylesheet" href="{!! url('/') !!}/assets/vodeacore/vendors/ladda/css/ladda-themeless.min.css">
+<link rel="stylesheet" href="{!! url('/') !!}/assets/vodea/vodeacore/vendors/ladda/css/ladda-themeless.min.css">
 <!-- End plugin css for this page -->
 <!-- inject:css -->
-<link rel="stylesheet" href="{!! url('/') !!}/assets/vodeacore/css/master.css">
+<link rel="stylesheet" href="{!! url('/') !!}/assets/vodea/vodeacore/css/master.css">
 <!-- endinject -->
 <!-- Layout styles -->
-<link rel="stylesheet" href="{!! url('/') !!}/assets/vodeacore/css/style.css">
-<link rel="stylesheet" href="{!! url('/') !!}/assets/vodeacore/css/vodeacore.css">
+<link rel="stylesheet" href="{!! url('/') !!}/assets/vodea/vodeacore/css/style.css">
+<link rel="stylesheet" href="{!! url('/') !!}/assets/vodea/vodeacore/css/vodeacore.css">
 <!-- End Layout styles -->
 
 <!-- custom style -->
--- src/views/cms/layouts/part/js.blade.php
+++ src/views/cms/layouts/part/js.blade.php
@@ -1,27 +1,27 @@
 <!-- plugins:js -->
-<script src="{!! url('/') !!}/assets/vodeacore/vendors/js/vendor.bundle.base.js"></script>
-<script src="{!! url('/') !!}/assets/vodeacore/vendors/js/vendor.bundle.addons.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/vendors/js/vendor.bundle.base.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/vendors/js/vendor.bundle.addons.js"></script>
 <!-- endinject -->
 <!-- Plugin js for this page-->
-<script src="{!! url('/') !!}/assets/vodeacore/vendors/tinymce/tinymce.min.js"></script>
-<script src="{!! url('/') !!}/assets/vodeacore/vendors/tinymce/themes/modern/theme.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/vendors/tinymce/tinymce.min.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/vendors/tinymce/themes/modern/theme.js"></script>
 
-<script src="{!! url('/') !!}/assets/vodeacore/vendors/ladda/js/ladda.jquery.min.js"></script>
-<script src="{!! url('/') !!}/assets/vodeacore/vendors/ladda/js/spin.min.js"></script>
-<script src="{!! url('/') !!}/assets/vodeacore/vendors/ladda/js/ladda.min.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/vendors/ladda/js/ladda.jquery.min.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/vendors/ladda/js/spin.min.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/vendors/ladda/js/ladda.min.js"></script>
 <!-- End plugin js for this page-->
 
 <!-- inject:js -->
-<script src="{!! url('/') !!}/assets/vodeacore/js/shared/off-canvas.js"></script>
-<script src="{!! url('/') !!}/assets/vodeacore/js/shared/hoverable-collapse.js"></script>
-<script src="{!! url('/') !!}/assets/vodeacore/js/shared/misc.js"></script>
-<script src="{!! url('/') !!}/assets/vodeacore/js/shared/settings.js"></script>
-<script src="{!! url('/') !!}/assets/vodeacore/js/shared/todolist.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/js/shared/off-canvas.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/js/shared/hoverable-collapse.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/js/shared/misc.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/js/shared/settings.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/js/shared/todolist.js"></script>
 <!-- endinject -->
 <!-- Custom js for this page-->
-<script src="{!! url('/') !!}/assets/vodeacore/js/dashboard.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/js/dashboard.js"></script>
 
-<script src="{!! url('/') !!}/assets/vodeacore/js/form-engine.js"></script>
+<script src="{!! url('/') !!}/assets/vodea/vodeacore/js/form-engine.js"></script>
 <!-- End custom js for this page-->
 
 

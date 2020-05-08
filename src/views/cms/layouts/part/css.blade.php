f8d09d4f2009d8cecfa19e9804fbbe234bfe4a8d
Yudi Hertanto <yudi@vodea.id>
2020-05-08 09:53:26 +0700

    change asset url

--- src/views/cms/layouts/part/css.blade.php
+++ src/views/cms/layouts/part/css.blade.php
@@ -1,24 +1,24 @@
 <!-- plugins:css -->
-@css(/assets/vodea/vodeacore/vendors/iconfonts/mdi/css/materialdesignicons.min.css)
-@css(/assets/vodea/vodeacore/vendors/iconfonts/puse-icons-feather/feather.css)
-@css(/assets/vodea/vodeacore/vendors/css/vendor.bundle.base.css)
-@css(/assets/vodea/vodeacore/vendors/css/vendor.bundle.addons.css)
+<link rel="stylesheet" href="{{ asset('/assets/vodea/vodeacore/vendors/iconfonts/mdi/css/materialdesignicons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/assets/vodea/vodeacore/vendors/iconfonts/puse-icons-feather/feather.css') }}">
+<link rel="stylesheet" href="{{ asset('/assets/vodea/vodeacore/vendors/css/vendor.bundle.base.css') }}">
+<link rel="stylesheet" href="{{ asset('/assets/vodea/vodeacore/vendors/css/vendor.bundle.addons.css') }}">
 <!-- endinject -->
 <!-- plugin css for this page -->
-@css(/assets/vodea/vodeacore/vendors/iconfonts/font-awesome/css/font-awesome.min.css)
+<link rel="stylesheet" href="{{ asset('/assets/vodea/vodeacore/vendors/iconfonts/font-awesome/css/font-awesome.min.css') }}">
 
-@css(/assets/vodea/vodeacore/vendors/ladda/css/ladda-themeless.min.css)
+<link rel="stylesheet" href="{{ asset('/assets/vodea/vodeacore/vendors/ladda/css/ladda-themeless.min.css') }}">
 
-@css(/assets/vodea/vodeacore/vendors/chartjs/Chart.min.css)
+<link rel="stylesheet" href="{{ asset('/assets/vodea/vodeacore/vendors/chartjs/Chart.min.css') }}">
 <!-- End plugin css for this page -->
 <!-- inject:css -->
-@css(/assets/vodea/vodeacore/css/master.css)
+<link rel="stylesheet" href="{{ asset('/assets/vodea/vodeacore/css/master.css') }}">
 <!-- endinject -->
 <!-- Layout styles -->
-@css(/assets/vodea/vodeacore/css/custom-bootstrap-margin-padding.css)
-@css(/assets/vodea/vodeacore/css/style.css)
-@css(/assets/vodea/vodeacore/css/vodeacore.css)
+<link rel="stylesheet" href="{{ asset('/assets/vodea/vodeacore/css/custom-bootstrap-margin-padding.css') }}">
+<link rel="stylesheet" href="{{ asset('/assets/vodea/vodeacore/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('/assets/vodea/vodeacore/css/vodeacore.css') }}">
 <!-- End Layout styles -->
 
 <!-- custom style -->
-@css(/assets/admin/css/style.css)
+<link rel="stylesheet" href="{{ asset('/assets/admin/css/style.css') }}">
